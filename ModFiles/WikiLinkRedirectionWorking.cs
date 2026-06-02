91e223b2a61aec9c62a02d3f9b22083fe2dcc0cf
catboy <catboy1357a@gmail.com>
2026-06-02 08:27:31 -0400

    Adding links to file

--- ModFiles/WikiLinkRedirectionWorking.cs
+++ ModFiles/WikiLinkRedirectionWorking.cs
@@ -7,4 +7,5 @@ public partial class WikiLinks : ResoniteMod
 	public const string ModName = "WikiLinkRedirectionWorking";
 	public const string ModAuthor = "catboy";
 	public override string Version => "1.0.0";
+	public override string Link => "https://github.com/catboy1357/WikiLinksLinkToPages";
 }
